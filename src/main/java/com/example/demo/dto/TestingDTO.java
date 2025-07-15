844abebaf832c875f6061cb3d1b1ad7643b1634a
aswinikotlo <aswinikotlo@localhost>
2025-07-15 20:50:03 +0530

    taskId :testing dto

---
+++ src/main/java/com/example/demo/dto/TestingDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+public class TestingDTO {
+	
+	private int testValu;
+
+	public int getTestValu() {
+		return testValu;
+	}
+
+	public void setTestValu(int testValu) {
+		this.testValu = testValu;
+	}
+
+}
