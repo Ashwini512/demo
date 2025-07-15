f663480c547371e3daeb13f0e6d539ca664ecee9
Bhagya Sree Jarugu <jbsree019@gmail.com>
2025-07-15 20:30:23 +0530

    DTO Changes

---
+++ src/main/java/com/example/demo/dto/StatusDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+public class StatusDTO {
+
+	private String  statusOfTopic;
+
+	public String getStatusOfTopic() {
+		return statusOfTopic;
+	}
+
+	public void setStatusOfTopic(String statusOfTopic) {
+		this.statusOfTopic = statusOfTopic;
+	}
+	
+}
